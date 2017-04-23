8fa8459a412a6a63c16573caef4f37950b528f53
Foxi352 <serge@swa.lu>
2017-04-23 15:59:27 +0200

    Adding entrypoint script

---
+++ entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [ $# -eq 0 ]; then
+    ARGS=/etc/knxd/knxd.ini
+else
+    ARGS=$@
+fi
+CONFIG_PATH=/etc/knxd
+
+if [ ! -e "$CONFIG_PATH/knxd.ini" ]; then
+    echo "No config file found, using example file"
+    cp /root/knxd.ini $CONFIG_PATH/;
+fi
+
+chown knxd:knxd /etc/knxd/knxd.ini
+
+su -s /bin/sh -c 'knxd $ARGS' knxd
